69b6545201c9f3b7d8b1c12843fa63f3ab88ade6
IurkaAntia <yuraantia@gmail.com>
2025-05-23 00:34:47 +0400

    Added 6 modified 24

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
 
-Route::get('home', function () {
-    return Inertia::render('Home');
-})->middleware('guest')->name('home');
+//Route::get('home', function () {
+//    return Inertia::render('Home');
+//})->middleware('guest')->name('home');
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
