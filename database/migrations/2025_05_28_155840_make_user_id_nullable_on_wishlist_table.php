f2e6ff7905df75c0bbe16b5749d265cf32452d94
IurkaAntia <yuraantia@gmail.com>
2025-05-31 13:04:32 +0400

    Fix fixing bugs N:13

--- database/migrations/2025_05_28_155840_make_user_id_nullable_on_wishlist_table.php
+++ database/migrations/2025_05_28_155840_make_user_id_nullable_on_wishlist_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('wishlist', function (Blueprint $table) {
+        Schema::table('wishlist', function (Blueprint $table) {
             $table->id();
 
             $table->foreignId('user_id')
